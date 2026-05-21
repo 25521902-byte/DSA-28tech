9c85356e1a05192ffc6e680d2ad6319d5900fb8a
Huỳnh Minh Trí <25521902@gm.uit.edu.vn>
2026-05-21 18:01:59 +0700

    Done

---
+++ Vector & Iterator (C++)/17.Số nhị phân.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<char> binary_converting(long long value)
+{
+    vector<char> result;
+    for (int i = 0; i < 64; i++)
+    {
+        int remainder = value % 2;
+        result.push_back(remainder + '0');
+        value /= 2;
+    }
+    reverse(result.begin(), result.end());
+    return result;
+}
+
+void querry(int t)
+{
+    for (int i = 0; i < t; i++)
+    {
+        long long value;
+        cin >> value;
+        vector<char> binary_number = binary_converting(value);
+        for (const auto &x : binary_number)
+            cout << x;
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    querry(t);
+    return 0;
+}
